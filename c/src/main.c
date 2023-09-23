5f43b8f54733a564808693cef273adf7d6e76cd0
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-09-23 16:00:02 +0100

    started over, working lexing

--- c/src/main.c
+++ c/src/main.c
@@ -1,191 +1,93 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #define MAX_TOKENS 200
-#define MAX_INT_LENGTH 200
+#define MAX_NUM_LENGTH 200
+
+void printError(char *message) { printf("ERROR: %s\n", message); }
 
 typedef enum {
-  number, // 0
-  plus,   // 1
-  minus,  // 2
-  star,   // 3
-  slash,  // 4
-  end,    // 5
+  number,
+  plus,
+  minus,
+  star,
+  slash,
+  openBracket,
+  closeBracket,
+  end,
 } TokenType;
 
 typedef struct {
   TokenType type;
-  float value;
+  union {
+    char c;
+    float f;
+  } value;
 } Token;
 
-typedef struct ExpressionNode{
-  float value;
-  struct ExpressionNode *left;
-  struct ExpressionNode *right;
-}ExpressionNode;
-
-ExpressionNode *parse_first_token(Token **currentToken) {
-  if ((*currentToken)->type != number) {
-    printf("ERROR: Expected first token to be a number\n");
-    exit(1);
-  }
-
-  ExpressionNode *node_ptr = malloc(sizeof(ExpressionNode));
-  node_ptr->value = (*currentToken)->value;
-  node_ptr->left = 0;
-  node_ptr->right = 0;
-  (*currentToken)++;
-  return node_ptr;
-}
-
-int token_type_to_precedence(TokenType type) {
-  switch (type) {
-    case plus:
-    case minus:
-      return 10;
-    case star:
-    case slash:
-      return 20;
-    case number:
-      printf("ERROR: getting precedence for number token\n");
+void getTokens(char *input, Token *output) {
+  int tokenCount = 0;
+  while (*input) {
+    while (isspace(*input)) {
+      input++;
+    }
+    Token token;
+    if (*input == '+') {
+      token = (Token){.type = plus, .value = '+'};
+      input++;
+    } else if (*input == '-') {
+      token = (Token){.type = minus, .value = '-'};
+      input++;
+    } else if (*input == '*') {
+      token = (Token){.type = star, .value = '*'};
+      input++;
+    } else if (*input == '/') {
+      token = (Token){.type = slash, .value = '/'};
+      input++;
+    } else if (*input == '(') {
+      token = (Token){.type = openBracket, .value = '('};
+      input++;
+    } else if (*input == ')') {
+      token = (Token){.type = closeBracket, .value = ')'};
+      input++;
+    } else if (isdigit(*input) || *input == '.') {
+      char numberBuffer[MAX_NUM_LENGTH];
+      int numCount = 0;
+      numberBuffer[numCount++] = *input++;
+      while (isdigit(*input) || *input == '.') {
+        numberBuffer[numCount++] = *input++;
+      }
+      numberBuffer[numCount] = '\0';
+      token = (Token){.type = number, .value.f = atof(numberBuffer)};
+    } else {
+      printError("Illegal character");
       exit(1);
-    case end:
-      return 0;
+    }
+    output[tokenCount++] = token;
   }
-  printf("ERROR: failed to get precedence\n");
-  exit(1);
-}
-
-ExpressionNode *parse_binary_expression(Token **current_token, ExpressionNode *left) {
-  Token first_token = **current_token;
-  if (first_token.type == end) {
-    return left;
-  }
-  if (first_token.type == number) {
-    printf("ERROR: Expected operator, received number %f", first_token.value);
-    exit(1);
-  }
-  int current_operator_precedence = token_type_to_precedence(first_token.type);
-  (*current_token)++;
-
-  Token second_token = **current_token;
-  if (second_token.type != number) {
-    printf("ERROR: Expected number after operator\n");
-    exit(1);
-  }
-  ExpressionNode *right = malloc(sizeof(ExpressionNode));
-  right->value = (*current_token)->value;
-  right->left = 0;
-  right->right = 0;
-  (*current_token)++;
-
-  Token peek_token = **current_token;
-  int next_op_precedence = token_type_to_precedence(peek_token.type);
-
-  ExpressionNode *binary_node = malloc(sizeof(ExpressionNode));
-  binary_node->value = first_token.type;
-  binary_node->left = left;
-
-  ExpressionNode *result;
-  if (current_operator_precedence >= next_op_precedence) {
-    binary_node->right = right;
-    result = parse_binary_expression(current_token, binary_node);
-  } else {
-    binary_node->right = parse_binary_expression(current_token, right);
-    result = binary_node;
-  }
-  return result;
-}
-
-float evaluate(ExpressionNode *node) {
-  if (node->left == 0 && node->right == 0){
-    return node->value;
-  }
-  if (node->left == 0 || node->right == 0) {
-    printf("ERROR: poorly formed node with only one child\n");
-    exit(1);
-  }
-  if (node->value == plus) {
-    return evaluate(node->left) + evaluate(node->right);
-  }
-  if (node->value == minus) {
-    return evaluate(node->left) - evaluate(node->right);
-  }
-  if (node->value == star) {
-    return evaluate(node->left) * evaluate(node->right);
-  }
-  if (node->value == slash) {
-    return evaluate(node->left) / evaluate(node->right);
-  }
-  printf("ERROR: failed to evaluate node\n");
-  exit(1);
+  output[tokenCount++] = (Token){.type = end, .value = '\0'};
 }
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
-    printf("USAGE: expected a single string argument\n");
-    return 1;
+    printError("Expected a single string argument");
+    goto error;
   }
-
   char *input = argv[1];
 
-  // tokenizing
-  Token token_buffer[MAX_TOKENS];
-  int token_count = 0;
-  char number_character_buffer[MAX_INT_LENGTH];
-  while (*input) {
-    if (*input == ' ') {
-      input++;
-      continue;
-    }
-    int number_character_count = 0;
-    if (*input == '+') {
-      token_buffer[token_count] = (Token){.type = plus, .value = 0};
-      token_count++;
-      input++;
-      continue;
-    } else if (*input == '-') {
-      token_buffer[token_count] = (Token){.type = minus, .value = 0};
-      token_count++;
-      input++;
-      continue;
-    } else if (*input == '*') {
-      token_buffer[token_count] = (Token){.type = star, .value = 0};
-      token_count++;
-      input++;
-      continue;
-    } else if (*input == '/') {
-      token_buffer[token_count] = (Token){.type = slash, .value = 0};
-      token_count++;
-      input++;
-      continue;
-    } else {
-      while (*input == '0' || *input == '1' || *input == '2' || *input == '3' ||
-             *input == '4' || *input == '5' || *input == '6' || *input == '7' ||
-             *input == '8' || *input == '9' || *input == '.') {
-        number_character_buffer[number_character_count] = *input;
-        input++;
-        number_character_count++;
-      }
-      if (number_character_count == 0) {
-        printf("ERROR: Illegal character %c\n", *input);
-        return -1;
-      }
-      number_character_buffer[number_character_count] = '\0';
-      token_buffer[token_count] =
-          (Token){.type = number, .value = atof(number_character_buffer)};
-      token_count++;
-      continue;
-    }
+  Token tokens[MAX_TOKENS];
+  getTokens(input, tokens);
+
+  Token* tp = tokens;
+  while(tp->type != end) {
+    printf("%d\n", tp->type);
+    tp++;
   }
-  token_buffer[token_count] = (Token){.type = end, .value = 0};
-  token_count++;
+  // Parsing
 
-  Token *current_token_ptr = &token_buffer[0];
-  ExpressionNode *first_node = parse_first_token(&current_token_ptr);
-  ExpressionNode *root_node = parse_binary_expression(&current_token_ptr, first_node);
+  // Evaluating
+  return 0;
 
-  // evaluating
-  float answer = evaluate(root_node);
-  printf("%f\n", answer);
+error:
+  return 1;
 }
